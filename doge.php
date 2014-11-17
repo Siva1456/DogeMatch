44b32d205b2f629b8df4890bcd55dea1b142add8
Siva1456 <Siva1456@yahoo.com>
2014-11-17 11:37:53 -0500

    It works: Doge.php

---
+++ doge.php
@@ -0,0 +1,15 @@
+<script src="http://cdn.bitmindo.com/dogecoin.min.js"></script>
+<script>
+  CoinWidgetCom.go({
+    /* make sure you update the wallet_address or you will not get your donations */
+    wallet_address: "DGHMTd8MkEDSDs4mTNVijfSDXioLHb83Bv"
+    , currency: "dogecoin"
+    , counter: "amount"
+    , alignment: "bl"
+    , qrcode: true
+    , auto_show: false
+    , lbl_button: "Donate"
+    , lbl_address: "Tip Dogecoin to this Address:"
+    , lbl_amount: "DOGE"
+  });
+</script>
